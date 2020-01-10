7fa540737fa322ddb8224332df4fe57a3d2def3c
dsoljan <soljan.domina@gmail.com>
2020-01-10 17:21:48 +0100

    Blog search minor fixes

--- src/components/textSearch.js
+++ src/components/textSearch.js
@@ -2,42 +2,26 @@ import React from "react"
 import { graphql, StaticQuery } from "gatsby"
 import Search from "./lunrSearch"
 import ReactWOW from "react-wow"
-<<<<<<< HEAD
-import { MDBIcon } from "mdbreact"
-=======
-import { MDBIcon } from "mdbreact";
-import { GoSearch } from "react-icons/go";
-
-
->>>>>>> 1059ee7a27e115e7a741c33dc2844fa01a6f8f12
+import { GoSearch } from "react-icons/go"
 
 export default () => {
   return (
-    <StaticQuery
-      query={graphql`
-        {
-          site {
-            siteMetadata {
-              blogPosts {
-                path
-              }
-            }
-          }
-          siteSearchIndex {
-            index
-          }
-        }
-      `}
+    <StaticQuery  
+    query={graphql`  
+        query SearchIndexQuery {  
+          siteSearchIndex {  
+            index  
+          }  
+        }  
+      `}  
       render={data => (
-        
         <React.Fragment>
-<<<<<<< HEAD
           <nav
             class="navbar navbar-dark "
             style={{
               backgroundColor: "#00c851",
-              marginTop: "150px",
-              marginBottom: "20px",
+              marginTop: "8em",
+              marginBottom: "3em",
             }}
           >
             <ReactWOW animation="fadeIn">
@@ -46,8 +30,13 @@ export default () => {
               </div>
             </ReactWOW>
             <form class="form-inline">
-              <div class="md-form my-0">
-                <MDBIcon icon="search" />
+              <div
+                class="md-form my-0"
+                style={{ display: "flex", paddingTop: "1em" }}
+              >
+                <div style={{ color: "white" }}>
+                  <GoSearch />
+                </div>
                 <Search searchIndex={data.siteSearchIndex.index} />
               </div>
             </form>
@@ -61,40 +50,6 @@ export default () => {
             }
           `}</style>
         </React.Fragment>
-=======
-        <nav
-          class="navbar navbar-dark "
-          style={{ 
-            backgroundColor: "#00c851",
-            marginTop: "8em",
-            marginBottom: "3em"
-        }}
-        >
-          <ReactWOW animation="fadeIn">
-            <div className="h1" style={{ color: "white" }}>
-              Blog
-            </div>
-          </ReactWOW>
-          <form class="form-inline">
-            <div class="md-form my-0" style = {{display: "flex", paddingTop: "1em"}}>
-            <div style = {{color: "white"}}>
-            <GoSearch/>
-            </div>
-            <Search searchIndex={data.siteSearchIndex.index}/>
-            </div>
-            
-          </form>
-        </nav>
-        <style jsx>{`
-        a {
-            color: white !important;
-        }
-        search {
-            color: white !important;
-        }
-        `}</style>
-      </React.Fragment>
->>>>>>> 1059ee7a27e115e7a741c33dc2844fa01a6f8f12
       )}
     />
   )
