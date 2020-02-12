7f0c450b57f3c9df575032c1dd93e9b6659870fd
dsoljan <soljan.domina@gmail.com>
2020-02-12 11:23:22 +0100

    Fixed build errors

--- gatsby-node.js
+++ gatsby-node.js
@@ -48,3 +48,19 @@ exports.createPages = ({ graphql, boundActionCreators }) => {
     })
   })
 }
+exports.onCreateWebpackConfig = ({ stage, rules, loaders, actions }) => {
+  switch (stage) {
+    case 'build-html':
+      actions.setWebpackConfig({
+        module: {
+          rules: [
+            {
+              test: /react-leaflet/,
+              use: [loaders.null()]
+            }
+          ]
+        }
+      });
+      break;
+  }
+};
--- src/components/leafletMap.js
+++ src/components/leafletMap.js
@@ -1,22 +1,24 @@
-import React from 'react'
-import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
-import '../styles/leafletMap.css'
-
+import React from "react"
+import { Map, TileLayer, Marker, Popup } from "react-leaflet"
+import "../styles/leafletMap.css"
 
 class LeafletMap extends React.Component {
-
   render() {
-      return (
-        <Map center = {this.props.position} zoom = {this.props.zoom}>
-          <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            attribution = "&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-          />
-          {this.props.markerText !== "" &&
-          <Marker position={[43.5313269,16.5261697]}></Marker>
-          }
-        </Map>
-      );
+    return (
+      <div ref={this.mapWrapperRef} className="map-wrapper">
+        {typeof window !== "undefined" ? (
+          <Map center={this.props.position} zoom={this.props.zoom}>
+            <TileLayer
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            />
+            {this.props.markerText !== "" && (
+              <Marker position={[43.5313269, 16.5261697]}></Marker>
+            )}
+          </Map>
+        ) : null}
+      </div>
+    )
   }
 }
 export default LeafletMap
